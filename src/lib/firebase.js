1581fe5211f392229369eb8a66a3a8de700fb1b9
ggguille <g.granado.g@gmail.com>
2021-05-01 12:11:42 +0200

    remove firebase log and start logging web vitals

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,6 +7,4 @@ import config from '../config/firebase';
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
 
-console.log('firebase', firebase);
-
 export { firebase, FieldValue };
