a4c7410680b3c6a99176d8d1fa6eea432fb6442f
ggguille <g.granado.g@gmail.com>
2021-05-01 12:02:12 +0200

    refactor firebase config

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,7 +2,7 @@ import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-import config from './firebaseConfig';
+import config from '../../config/firebase';
 
 const firebase = Firebase.initializeApp(config);
 const { FieldValue } = Firebase.firestore;
