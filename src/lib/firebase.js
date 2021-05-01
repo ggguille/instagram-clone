633f3a556d1d192a3fc273b7c56b33824966afe1
Guille <g.granado.g@gmail.com>
2021-05-01 12:09:13 +0200

    Firebase (#2)
    * config and implement firebase
    
    * initial data
    
    * refactor firebase config
    
    * fix src imports

---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import config from '../config/firebase';
+
+const firebase = Firebase.initializeApp(config);
+const { FieldValue } = Firebase.firestore;
+
+console.log('firebase', firebase);
+
+export { firebase, FieldValue };
