813eb4613acf03f1e96a6796ad1530679378c76c
ggguille <g.granado.g@gmail.com>
2021-05-02 16:24:33 +0200

    create login page layout

--- src/pages/login.js
+++ src/pages/login.js
@@ -18,5 +18,14 @@ export default function Login() {
     document.title = 'Login - Instagram Clone';
   });
 
-  return <p>I am the login page</p>;
+  return (
+    <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+      <div className="flex w-3/5">
+        <img src="/images/iphone-with-profile.jpeg" alt="Iphone with Instagram Clone" />
+      </div>
+      <div className="flex flex-col w-2/5">
+        <p>I will be the form</p>
+      </div>
+    </div>
+  );
 }
