dd3e6d9e6ee7037032f412a74d5dca697386d87b
ggguille <g.granado.g@gmail.com>
2021-05-02 17:57:42 +0200

    create login form

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,6 +27,35 @@ export default function Login() {
         <h1 className="flex justify-center w-full">
           <img className="mt-2 w-6/12 mb-4" src="/images/logo.png" alt="Instagram Clone" />
         </h1>
+
+        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+
+        <form onSubmit={handleLogin} method="POST">
+          <input
+            aria-label="Enter your email address"
+            type="text"
+            placeholder="Email address"
+            className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
+            onChange={({ target }) => setEmailAddress(target.value)}
+            value={emailAddress}
+          />
+          <input
+            aria-label="Enter your password"
+            type="password"
+            placeholder="Password"
+            className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
+            onChange={({ target }) => setPassword(target.value)}
+            value={password}
+          />
+          <button
+            disabled={isInvalid}
+            type="submit"
+            className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+            ${isInvalid && 'opacity-50'}`}
+          >
+            Log In
+          </button>
+        </form>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
   purge: {
     content: ['./src/**/*.js', './src/**/**/*.js']
   },
-  theme: {},
-  variants: {}
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          medium: '#005c98'
+        },
+        gray: {
+          base: '#616161',
+          background: '#fafafa',
+          primary: '#dbdbdb'
+        },
+        red: {
+          primary: '#ed4956'
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  }
 };
