4a361c2cfe9dd88621a69d5aba256ffd525b954a
ggguille <g.granado.g@gmail.com>
2021-05-02 18:21:31 +0200

    close login page template

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -24,38 +25,48 @@ export default function Login() {
         <img src="/images/iphone-with-profile.jpeg" alt="Iphone with Instagram Clone" />
       </div>
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img className="mt-2 w-6/12 mb-4" src="/images/logo.png" alt="Instagram Clone" />
-        </h1>
+        <div className="flex flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img className="mt-2 w-6/12 mb-4" src="/images/logo.png" alt="Instagram Clone" />
+          </h1>
 
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
-            onChange={({ target }) => setEmailAddress(target.value)}
-            value={emailAddress}
-          />
-          <input
-            aria-label="Enter your password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
-            onChange={({ target }) => setPassword(target.value)}
-            value={password}
-          />
-          <button
-            disabled={isInvalid}
-            type="submit"
-            className={`bg-blue-medium text-white w-full rounded h-8 font-bold
-            ${isInvalid && 'opacity-50'}`}
-          >
-            Log In
-          </button>
-        </form>
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
+              onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress}
+            />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 mb-2 py-5 px-4 h-2 border border-gray-primary rounded"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
+            />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+              ${isInvalid && 'opacity-50'}`}
+            >
+              Log In
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to={ROUTES.SIGNUP} className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
