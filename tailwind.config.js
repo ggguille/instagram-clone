5f95939900aa00d18cf0e568cfb7d62fa5e29539
ggguille <g.granado.g@gmail.com>
2021-05-06 20:59:37 +0200

    change tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,10 @@ module.exports = {
         blue: {
           medium: '#005c98'
         },
+        black: {
+          light: '#262626',
+          faded: '#00000059'
+        },
         gray: {
           base: '#616161',
           background: '#fafafa',
