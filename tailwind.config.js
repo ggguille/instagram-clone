a5e516f0209281241adbff6c4c90891ee481e671
Guille <g.granado.g@gmail.com>
2021-05-06 21:11:34 +0200

    Login page (#7)
    * init login logic
    
    * create login page layout
    
    * add app logo
    
    * create login form
    
    * close login page template
    
    * create sign up page
    
    * handle login
    
    * change tailwindcss colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,28 @@ module.exports = {
   purge: {
     content: ['./src/**/*.js', './src/**/**/*.js']
   },
-  theme: {},
-  variants: {}
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          medium: '#005c98'
+        },
+        black: {
+          light: '#262626',
+          faded: '#00000059'
+        },
+        gray: {
+          base: '#616161',
+          background: '#fafafa',
+          primary: '#dbdbdb'
+        },
+        red: {
+          primary: '#ed4956'
+        }
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  }
 };
