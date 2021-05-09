b6394c9c685998a577215239fe2e6747c025509a
ggguille <g.granado.g@gmail.com>
2021-05-09 15:04:56 +0200

    create useUser hook

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -8,3 +8,12 @@ export async function doesUsernameExists(username) {
     .get();
   return result.docs.length > 0;
 }
+
+export async function getUserByUserId(userId) {
+  const result = await firebase.firestore().collection('users').where('userId', '==', userId).get();
+  const user = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id
+  }));
+  return user;
+}
