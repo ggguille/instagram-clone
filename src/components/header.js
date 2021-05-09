80519179f28f1441916afc8bec77d26bb44dabde
ggguille <g.granado.g@gmail.com>
2021-05-09 16:14:26 +0200

    add default avatar image if not found in header

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
+import { DEFAULT_IMAGE_PATH } from '../constants/paths';
 
 export default function Header() {
   const { firebase } = useContext(FirebaseContext);
@@ -74,6 +75,9 @@ export default function Header() {
                       className="rounded-full h-8 w-8 object-cover"
                       src={`/images/avatars/${user.displayName}.jpeg`}
                       alt={user.displayName}
+                      onError={(e) => {
+                        e.target.src = DEFAULT_IMAGE_PATH;
+                      }}
                     />
                   </Link>
                 </div>
