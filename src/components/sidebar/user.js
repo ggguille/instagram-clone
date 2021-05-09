c28d4dc6096926f52fc5349382acee25cd3ce5a8
ggguille <g.granado.g@gmail.com>
2021-05-09 15:37:09 +0200

    create Sidebar.User component

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -1,3 +1,34 @@
-export default function User() {
-  return <p>USER</p>;
-}
+import PropTypes from 'prop-types';
+import { memo } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { Link } from 'react-router-dom';
+import { DEFAULT_IMAGE_PATH } from '../../constants/paths';
+
+const User = ({ username, fullName }) =>
+  !username || !fullName ? (
+    <Skeleton count={1} height={61} />
+  ) : (
+    <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
+      <div className="flex items-center justify-between col-span-1">
+        <img
+          className="rounded-full w-16 flex mr-3"
+          src={`/images/avatars/${username}.jpg`}
+          alt=""
+          onError={(e) => {
+            e.target.src = DEFAULT_IMAGE_PATH;
+          }}
+        />
+      </div>
+      <div className="col-span-3">
+        <p className="font-bold text-sm">{username}</p>
+        <p className="text-sm">{fullName}</p>
+      </div>
+    </Link>
+  );
+
+User.propTypes = {
+  username: PropTypes.string,
+  fullName: PropTypes.string
+};
+
+export default memo(User);
