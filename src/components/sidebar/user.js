14b38ff4c5da7855a1779b30018ebbd930fd66b9
ggguille <g.granado.g@gmail.com>
2021-05-09 16:04:03 +0200

    finish Sidebar.User component

--- src/components/sidebar/user.js
+++ src/components/sidebar/user.js
@@ -11,8 +11,8 @@ const User = ({ username, fullName }) =>
     <Link to={`/p/${username}`} className="grid grid-cols-4 gap-4 mb-6 items-center">
       <div className="flex items-center justify-between col-span-1">
         <img
-          className="rounded-full w-16 flex mr-3"
-          src={`/images/avatars/${username}.jpg`}
+          className="rounded-full h-16 w-16 mr-3 object-cover"
+          src={`/images/avatars/${username}.jpeg`}
           alt=""
           onError={(e) => {
             e.target.src = DEFAULT_IMAGE_PATH;
@@ -27,8 +27,8 @@ const User = ({ username, fullName }) =>
   );
 
 User.propTypes = {
-  username: PropTypes.string,
-  fullName: PropTypes.string
+  username: PropTypes.string.isRequired,
+  fullName: PropTypes.string.isRequired
 };
 
 export default memo(User);
