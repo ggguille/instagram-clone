d27b8d20b2d806d83a53d948b4aa823f70904f9a
ggguille <g.granado.g@gmail.com>
2021-05-25 23:16:56 +0200

    following and followers firebase implementation

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,4 @@
-import { firebase } from '../lib/firebase';
+import { FieldValue, firebase } from '../lib/firebase';
 
 export async function doesUsernameExists(username) {
   const result = await firebase
@@ -23,4 +23,36 @@ export async function getSuggestedProfiles(userId, following) {
   return result.docs
     .map((user) => ({ ...user.data(), docId: user.id }))
     .filter((profile) => profile.userId !== userId && !following.includes(profile.userId));
+}
+
+export async function updateLoggedInUserFollowing(
+  loggedInUserDocId, // currently logged in user document id (karl's profile)
+  profileId, // the user that karl requests to follow
+  isFollowingProfile // true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(loggedInUserDocId)
+    .update({
+      following: isFollowingProfile
+        ? FieldValue.arrayRemove(profileId)
+        : FieldValue.arrayUnion(profileId)
+    });
+}
+
+export async function updateFollowedUserFollowers(
+  profileDocId, // currently logged in user document id (karl's profile)
+  loggedInUserDocId, // the user that karl requests to follow
+  isFollowingProfile // true/false (am i currently following this person?)
+) {
+  return firebase
+    .firestore()
+    .collection('users')
+    .doc(profileDocId)
+    .update({
+      followers: isFollowingProfile
+        ? FieldValue.arrayRemove(loggedInUserDocId)
+        : FieldValue.arrayUnion(loggedInUserDocId)
+    });
 }
