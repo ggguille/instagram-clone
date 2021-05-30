aee66d800a2b11de8759292152da214c7e7a31fd
ggguille <g.granado.g@gmail.com>
2021-05-30 12:29:53 +0200

    loading timeline photos and build an skeleton

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,3 +1,23 @@
+import Skeleton from 'react-loading-skeleton';
+import usePhotos from '../hooks/use-photos';
+
 export default function Timeline() {
-  return <p>I am the timeline</p>;
+  const { photos } = usePhotos();
+  console.log('photos', photos);
+
+  if (!photos) {
+    return (
+      <>
+        {[...new Array(4)].map((_, index) => (
+          <Skeleton key={index} count={1} width={320} height={400} />
+        ))}
+      </>
+    );
+  }
+
+  if (photos.length === 0) {
+    return null;
+  }
+
+  return photos.map((content) => <p key={content.docId}>{content.imageSrc}</p>);
 }
