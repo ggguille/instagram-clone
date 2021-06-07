ab4483581b260676475269ee307f1e1effcd532a
ggguille <g.granado.g@gmail.com>
2021-06-07 21:58:24 +0200

    add preotected route and profile route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import * as ROUTES from './constants/routes';
 import UserContext from './context/user';
+import ProtectedRoute from './helpers/protected-route';
 import useAuthListener from './hooks/use-auth-listener';
 
 const Dashboard = lazy(() => import('./pages/dashboard'));
 const Login = lazy(() => import('./pages/login'));
 const Signup = lazy(() => import('./pages/signup'));
 const NotFound = lazy(() => import('./pages/not-found'));
+const Profile = lazy(() => import('./pages/profile'));
 
 export default function App() {
   const { user } = useAuthListener();
@@ -16,7 +18,10 @@ export default function App() {
       <Router>
         <Suspense fallback={<p>Loading...</p>}>
           <Switch>
-            <Route path={ROUTES.DASHBOARD} exact component={Dashboard} />
+            <ProtectedRoute user={user} path={ROUTES.DASHBOARD} exact>
+              <Dashboard />
+            </ProtectedRoute>
+            <Route path={ROUTES.PROFILE} component={Profile} />
             <Route path={ROUTES.LOGIN} component={Login} />
             <Route path={ROUTES.SIGNUP} component={Signup} />
             <Route component={NotFound} />
