0e8225689c5126dc12bad6fa9ec83484099b5afd
ggguille <g.granado.g@gmail.com>
2021-06-20 11:12:46 +0200

    add logged in user context into dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,22 +1,32 @@
 import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/header';
 import Sidebar from '../components/sidebar';
 import Timeline from '../components/timeline';
+import useUser from '../hooks/use-user';
+import LoggedInUserContext from '../context/logged-in-user';
 
-export default function Dashboard() {
+export default function Dashboard({ user: loggedInUser }) {
+  const { user, setActiveUser } = useUser(loggedInUser.uid);
   useEffect(() => {
     document.main = 'Instagram';
   }, []);
 
   return (
-    <div className="bg-gray-background">
-      <Header />
-      <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-        <div className="container col-span-2">
-          <Timeline />
+    <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
+      <div className="bg-gray-background">
+        <Header />
+        <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
+          <div className="container col-span-2">
+            <Timeline />
+          </div>
+          <Sidebar />
         </div>
-        <Sidebar />
       </div>
-    </div>
+    </LoggedInUserContext.Provider>
   );
 }
+
+Dashboard.propTypes = {
+  user: PropTypes.object.isRequired
+};
